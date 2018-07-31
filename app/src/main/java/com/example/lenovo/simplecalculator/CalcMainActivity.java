dfd135dae34eee86038edbbf29cd67382ff0c102
rakibsarwer <rakibsarwar3@gmail.com>
2018-07-31 21:41:19 +0600

    Android Calculator design .

---
+++ app/src/main/java/com/example/lenovo/simplecalculator/CalcMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.simplecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalcMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calc_activity_calc_main);
+    }
+}
